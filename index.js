95eb4bcdfc245451b0581765813f0e7a56f4f345
Ivan.Orlov <ivan@morrr.com>
2015-04-27 21:08:02 +0300

    Typos

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ var _ = require('lodash');
 
 
 module.exports = {
-	chain: function() {
-		function(funcCollection, thisArg) {
-			return new Promise(function(resolve, reject) {
-				var promises = [];
-				_.each(funcCollection, function(f) {
-					promises.push(function(callback) {
-						var curFunction = f;
-						if (thisArg) {
-							curFunction = f.bind(thisArg);
-						}
-						new Promise(curFunction).then(function(res) {
-							callback(null, res);
-						}).catch(function(e) {
-							callback(e, null);
-						});
+	chain: function(funcCollection, thisArg) {
+		return new Promise(function(resolve, reject) {
+			var promises = [];
+			_.each(funcCollection, function(f) {
+				promises.push(function(callback) {
+					var curFunction = f;
+					if (thisArg) {
+						curFunction = f.bind(thisArg);
+					}
+					new Promise(curFunction).then(function(res) {
+						callback(null, res);
+					}).catch(function(e) {
+						callback(e, null);
 					});
 				});
-				async.series(promises, function(err, results) {
-					if (err !== undefined) {
-						return reject(err);
-					} else {
-						return resolve(results);
-					}
-				})
 			});
-		}
+			async.series(promises, function(err, results) {
+				if (err !== undefined) {
+					return reject(err);
+				} else {
+					return resolve(results);
+				}
+			})
+		});
 	}
 }
